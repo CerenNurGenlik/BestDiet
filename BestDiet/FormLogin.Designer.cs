92409a09d3d442d5f5c0146177974a1ac951a5ee
CerenNur <you@example.com>
2022-06-15 18:24:42 +0300

    login form girildi

--- BestDiet/FormLogin.Designer.cs
+++ BestDiet/FormLogin.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.lnkKayitOl = new System.Windows.Forms.LinkLabel();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(34, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label2.Location = new System.Drawing.Point(100, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Şifre:";
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnGirisYap.Location = new System.Drawing.Point(165, 467);
+            this.btnGirisYap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(162, 30);
+            this.btnGirisYap.TabIndex = 1;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            // 
+            // lnkKayitOl
+            // 
+            this.lnkKayitOl.AutoSize = true;
+            this.lnkKayitOl.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lnkKayitOl.Location = new System.Drawing.Point(208, 555);
+            this.lnkKayitOl.Name = "lnkKayitOl";
+            this.lnkKayitOl.Size = new System.Drawing.Size(73, 24);
+            this.lnkKayitOl.TabIndex = 2;
+            this.lnkKayitOl.TabStop = true;
+            this.lnkKayitOl.Text = "Kayıt Ol";
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtKullaniciAdi.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(165, 296);
+            this.txtKullaniciAdi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(249, 28);
+            this.txtKullaniciAdi.TabIndex = 3;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtSifre.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtSifre.Location = new System.Drawing.Point(165, 377);
+            this.txtSifre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(249, 28);
+            this.txtSifre.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            //this.pictureBox1.Image = global::Proje_Formlar.Properties.Resources.bestdiet;
+            this.pictureBox1.Location = new System.Drawing.Point(165, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 178);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GirisEkrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormLogin";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(500, 603);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.lnkKayitOl);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GirisEkrani";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGirisYap;
+        private System.Windows.Forms.LinkLabel lnkKayitOl;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
