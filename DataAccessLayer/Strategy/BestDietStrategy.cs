2bac612de4c2f040a2c2331aa4d7ef83f0c482a6
CerenNur <you@example.com>
2022-06-16 15:03:32 +0300

    strategy sport eklendi

--- DataAccessLayer/Strategy/BestDietStrategy.cs
+++ DataAccessLayer/Strategy/BestDietStrategy.cs
@@ -23,6 +23,26 @@ namespace DataAccessLayer.Strategy
             };
             context.Users.Add(user);
             context.SaveChanges();
+
+            List<Sport> sports = new List<Sport>();
+
+            sports.Add(new Sport() { SportName = "Koşu", BurningCalori = 565 });
+            sports.Add(new Sport() { SportName = "Yüzme", BurningCalori = 650 });
+            sports.Add(new Sport() { SportName = "Yürüyüş", BurningCalori = 500 });
+            sports.Add(new Sport() { SportName = "Bisiklet", BurningCalori = 600 });
+            sports.Add(new Sport() { SportName = "Tenis", BurningCalori = 450 });
+            sports.Add(new Sport() { SportName = "Futbol", BurningCalori = 588 });
+            sports.Add(new Sport() { SportName = "Pilates", BurningCalori = 200 });
+            sports.Add(new Sport() { SportName = "Basketbol", BurningCalori = 440 });
+            sports.Add(new Sport() { SportName = "Voleybol", BurningCalori = 294 });
+            sports.Add(new Sport() { SportName = "Golf", BurningCalori = 390 });
+            sports.Add(new Sport() { SportName = "Ağırlık Çalışması", BurningCalori = 440 });
+            
+
+
+            context.Sports.AddRange(sports);
+            
+
         }
     }
 }
