1ed98ac05822c010ff72b610f3959cb245128e19
Bugra Albayrak <albayrakbugraa@gmail.com>
2022-06-16 15:28:19 +0300

    Food Repo ekleme

---
+++ DataAccessLayer/Repositories/FoodCategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Model.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class FoodCategoryRepository
+    {
+        BestDietDbContext db;
+        public FoodCategoryRepository()
+        {
+            db = new BestDietDbContext();
+        }
+        public List<FoodCategory> GetFoodCategories()
+        {
+            return db.FoodCategories.ToList();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/FoodRepository.cs
@@ -0,0 +1,41 @@
+﻿using Model.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class FoodRepository
+    {
+        BestDietDbContext db;
+        public FoodRepository()
+        {
+            db = new BestDietDbContext();
+        }
+        public List<Food> GetFoods()
+        {
+            return db.Foods.Where(a => a.IsActive).ToList();
+        }
+        public List<Food> GetFoodsByCategory(int foodCategoryID)
+        {
+            return db.Foods.Where(a => a.FoodCategoryID == foodCategoryID && a.IsActive).ToList();
+        }
+        public Food GetByFoodID(int foodID)
+        {
+            return db.Foods.Find(foodID);
+        }
+        public bool Insert(Food food)
+        {
+            db.Foods.Add(food);
+            return db.SaveChanges() > 0;
+        }
+        public bool Delete(Food food)
+        {
+            Food deletedFood = db.Foods.Find(food.FoodID);
+            deletedFood.IsActive = false;
+            return db.SaveChanges() > 0;
+        }
+    }
+}
